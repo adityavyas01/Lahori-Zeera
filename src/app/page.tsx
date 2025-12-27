794de3f043a4b69401008ed334ffbb8d64a5448b
ADITYA VYAS <adityavyas670@gmail.com>
2025-12-27 08:55:48 +0000

    its now working fine but the hero section is scrolling up too rather as

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -113,7 +113,7 @@ export default function Home() {
             </div>
         </div>
 
-        <div className="relative z-10 bg-background">
+        <div className="relative z-10 bg-background -mt-[100vh]">
             <AboutSection />
             <FlavorsSection />
             <IngredientsSection />
